a8aca96c7342d05347f56ad4f24bb6d31fbba4ac
Jonathan Tello Carrasco <jonathantello.dev@gmail.com>
2024-01-03 18:05:06 -0300

    avance: creacion de caja numeracion pedidos y fecha

--- src/components/views/remitos/FormRemitos.jsx
+++ src/components/views/remitos/FormRemitos.jsx
@@ -1,11 +1,36 @@
-import { Container } from "react-bootstrap";
-
+import { Col, Row } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 
 const FormRemitos = () => {
   return (
-    <>
-    
-    </>
+    <Form className="container-fluid d-flex flex-column gap-2 align-items-center">
+      <Row>
+        <Col xs={12} md={6}>
+          <Form.Group className="d-flex gap-1 align-items-center">
+            <Form.Label>Pedido N°</Form.Label>
+            <Form.Control
+              type="text"
+              value={"001"}
+              className="w-50"
+            ></Form.Control>
+          </Form.Group>
+        </Col>
+        <Col xs={12} md={6}>
+          <Form.Group className="d-flex gap-1 align-items-center">
+            <Form.Label>Fecha</Form.Label>
+            <Form.Control
+            type="date"></Form.Control>
+          </Form.Group>
+        </Col>
+      </Row>
+      <Row>
+
+      </Row>
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
   );
 };
 
